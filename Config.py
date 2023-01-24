de1f28f9ca5df88d299d093a158bd5ed2c043e00
Sainaangel <114768321+Sainaangel@users.noreply.github.com>
2023-01-24 17:51:34 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -14,4 +14,4 @@ class Config(object):
     ASSISTANT_ID = int(os.environ.get("ASSISTANT_ID", "5467311248")) # telegram I'd not Username
     AUTO_LEAVE_TIME = int(os.environ.get("AUTO_LEAVE_ASSISTANT_TIME", "54000")) # in seconds
     AUTO_LEAVE = os.environ.get('AUTO_LEAVING_ASSISTANT', "True") # Change it to "True"
-    MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", "ENABLE")
+    
