a900292dea64c9bff42cb4b6bf01922ead612383
Sainaangel <114768321+Sainaangel@users.noreply.github.com>
2023-01-30 07:11:25 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -4,7 +4,7 @@ class Config(object):
     API_ID = int(os.environ.get("APP_ID", "6296490"))
     API_HASH = os.environ.get("API_HASH", "24385183c93a98ae4155c25d9f5f64b2")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "6080251945:AAFPZ-5EA-GpbWG1KuPd0ET2J_ehDzQ1PUs")
-    STRING_SESSION = os.environ.get("STRING_SESSION", "1AZWarzUBu2ynmLo_WnAq_ggysMw4OGc8bxXNzznqPCKIgIMECSjLdqRMLMbqsje4gt95YviFK5vk3Fo8R3XEeWMmqFsnihrzMF-UtWz5C2-XuPElg8a_lBZBLNIMu-G1q6-FapdDo32dJIJZSKwkIm4j-6HCc7plQYEJn65iF6xvqBnM3z6_tWK3J3X8xlkHvpVUniRGvbjanOPDA1XBmaOFyN-GjfVdl-2F38BguPO8RcdidnvG4p-eSvjvDZ4utfVp2iLj-6lcDG4RXgHnOl-Ggyv-exwL6-qcpL8QZVGrSSjdecjfvjG572TUjho2RvvZ5rKSiqVPUymejRusJyukWmyl_6M=")
+    STRING_SESSION = os.environ.get("STRING_SESSION", "1BJWap1sBu1WDNirSFt2zhK2dhSC6-p2lYSd_wM4uv1vwgUWEliHrqbLBOdMKI0dEm0ZfuXOFFtCB9R1kQSrZSn1aqNiqG-3pD-vljRbJJ8HSYTIHoA-JP0FLJkBZMrmj4juRid2VUG6yaR7m7P_dxcMgCtzghZgeb_AWS8SleFnXFj-73P8Hnv0a2BfaBd9DsfyzaeXvG-QzQNhZGgl_Aceq3v_9sWhBegg10XmdLtmOwy3Wo4OgaCONDeZExE26Vxi40QlZEm0bFGK_3rbSAmogrNl_Sr8qPQA376HNV4sbL62u2wXBPSpRYL-w4E5W55Oi3IiNYk0JKxJHs62z_ZpHfiS_6QI=")
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "TgFlashRobot")
     SUPPORT = os.environ.get("SUPPORT", "TgFlash") # Your Support
